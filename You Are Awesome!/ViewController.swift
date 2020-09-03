a30e7001f2f658f6065494af831eda300e47503f
Jagmohan Sidhu <jsidhu1@mail.sfsu.edu>
2020-09-02 21:46:20 -0700

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Jagmohan Singh on 9/2/20.
+//  Copyright © 2020 SFSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
